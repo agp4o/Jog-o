b64f4f36609ea2806d7a833470bde38aa6329a7c
Petrus-Lacerda <38383267+Petrus-Lacerda@users.noreply.github.com>
2019-03-23 15:27:53 +0000

    Add files via upload

---
+++ Assets/ClickBTC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickBTC : MonoBehaviour
+{
+    public static int BTCmine;
+    public int VerBTC;
+    public GameObject BTCTexto;
+
+    public void MineBTC()
+    {
+        BTCmine += 1;
+    }
+
+    public 
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        VerBTC = BTCmine;
+        BTCTexto.GetComponent<Text>().text = "Bitcoin: " + VerBTC;
+    }
+}
