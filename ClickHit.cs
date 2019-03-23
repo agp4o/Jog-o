14006471b34ab641e0b826ddb4f52de2ff690b9b
Petrus-Lacerda <38383267+Petrus-Lacerda@users.noreply.github.com>
2019-03-23 16:02:35 +0000

    Add files via upload

---
+++ ClickHit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickHit : MonoBehaviour
+{
+    public static int HitBuy;
+    public int VerHit;
+    public GameObject HitTexto;
+    public GameObject Log;
+
+    public void BuyHit()
+    {
+        if(ClickBTC.BTCmine == 0)
+        {
+            Log.GetComponent<Text>().text = "BitCoins insufuciente";
+        }
+        else
+        {
+            ClickBTC.BTCmine -= 1;
+            HitBuy += 1;
+        }
+
+    }
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        VerHit = HitBuy;
+        HitTexto.GetComponent<Text>().text = "HitCoin:" + VerHit;
+    }
+}
